68b805210abf3e6bc6a39082cd204cd1f83cc0f5
Esteban Cruz <cruzcr6@gmail.com>
2024-12-06 09:55:14 -0600

    Initial Commit

---
+++ 01_Convert_2_Kotlin/app/src/main/java/com/ecruzh/a01_convert_2_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ecruzh.a01_convert_2_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ecruzh.a01_convert_2_kotlin.ui.theme._01_Convert_2_KotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _01_Convert_2_KotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _01_Convert_2_KotlinTheme {
+        Greeting("Android")
+    }
+}
